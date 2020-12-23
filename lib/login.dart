f0d47d3be0cd888f4d4d9bee63c0b8a1fa72dbe1
Amey Sunu <ameysunu00719@gmail.com>
2020-12-23 23:16:32 +0300

    ui redesign

--- lib/login.dart
+++ lib/login.dart
@@ -1,10 +1,13 @@
+import 'package:dohack/introduction.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import 'home.dart';
+String name;
+String email;
+String imageUrl;
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -28,6 +31,17 @@ Future<String> signInWithGoogle() async {
   if (user != null) {
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(user.photoURL != null);
+
+    name = user.displayName;
+    email = user.email;
+    imageUrl = user.photoURL;
+
+    if (name.contains(" ")) {
+      name = name.substring(0, name.indexOf(" "));
+    }
 
     final User currentUser = _auth.currentUser;
     assert(user.uid == currentUser.uid);
@@ -109,7 +123,7 @@ class _LoginState extends State<Login> {
                             Navigator.of(context).push(
                               MaterialPageRoute(
                                 builder: (context) {
-                                  return Home();
+                                  return Introduction();
                                 },
                               ),
                             );
