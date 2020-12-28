75a1dd0c0af16adfb08424eab2ced5b586f2112c
Amey Sunu <ameysunu00719@gmail.com>
2020-12-28 12:12:53 +0300

    drop down added

--- lib/pages/mood.dart
+++ lib/pages/mood.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import '../login.dart';
 
 class Mood extends StatefulWidget {
   @override
@@ -7,6 +8,7 @@ class Mood extends StatefulWidget {
 }
 
 class _MoodState extends State<Mood> {
+  String dropdownValue = 'Neutral 😶';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +30,80 @@ class _MoodState extends State<Mood> {
           ],
         ),
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              children: [
+                Center(
+                    child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "Hey $name, How are you doing?",
+                    style: TextStyle(
+                        fontFamily: 'Gotham',
+                        color: Colors.white,
+                        fontSize: 20),
+                  ),
+                )),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    color: HexColor('#CA82CC'),
+                    child: Theme(
+                      data: Theme.of(context).copyWith(
+                        canvasColor: HexColor('#CA82CC'),
+                      ),
+                      child: ButtonTheme(
+                        alignedDropdown: true,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: DropdownButton<String>(
+                            value: dropdownValue,
+                            iconSize: 0,
+                            elevation: 16,
+                            style: TextStyle(color: Colors.white),
+                            underline: Container(
+                              height: 0,
+                              color: Colors.white,
+                            ),
+                            onChanged: (String newValue) {
+                              setState(() {
+                                dropdownValue = newValue;
+                              });
+                            },
+                            items: <String>[
+                              'Neutral 😶',
+                              'Awesome  😁 ',
+                              'Really good  😄 ',
+                              'Fine 😀 ',
+                              'Bad 🥺',
+                              'Terrible  😩 ',
+                              'Sad  😭 '
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Text(value.toUpperCase(),
+                                      style: TextStyle(
+                                          fontFamily: 'Gotham',
+                                          color: Colors.white,
+                                          fontSize: 20)),
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ));
   }
 }
