43e5aeb8e4e034284eb8d5dfc0ff58e16ad791cd
Amey Sunu <ameysunu00719@gmail.com>
2020-12-29 13:29:22 +0300

    ui fixes

--- lib/pages/weekly.dart
+++ lib/pages/weekly.dart
@@ -12,22 +12,93 @@ class _WeekState extends State<Week> {
     return Scaffold(
       backgroundColor: HexColor('#CA82CC'),
       body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Center(
-                child: Padding(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Text(
-                    "Avicii".toUpperCase(),
+                    "Doing Well",
                     style: TextStyle(
                         fontFamily: 'Gotham',
                         color: Colors.white,
                         fontSize: 20),
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text(
+                                "This week".toUpperCase(),
+                                style: TextStyle(
+                                    fontFamily: 'Gotham', fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "20 DEC - 27 DEC".toUpperCase(),
+                            style: TextStyle(
+                                fontFamily: 'Gotham',
+                                fontSize: 15,
+                                color: Colors.black54),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    width: MediaQuery.of(context).size.width * 1,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: HexColor('#FFE7FF'),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                            child: Text(
+                          "3",
+                          style: TextStyle(fontFamily: 'Gotham', fontSize: 50),
+                        )),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "blogged days".toLowerCase(),
+                            style: TextStyle(
+                                fontFamily: 'Gotham',
+                                fontSize: 17,
+                                color: Colors.black54),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
