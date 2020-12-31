fd66a66d0ae41a525c2c0a1d3c16edd42a3c6d43
Amey Sunu <ameysunu00719@gmail.com>
2020-12-31 14:42:22 +0300

    firestore added

--- lib/pages/viewblog.dart
+++ lib/pages/viewblog.dart
@@ -2,6 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
+final firestoreInstance = FirebaseFirestore.instance;
+AsyncSnapshot<DocumentSnapshot> snapshot;
+Stream<QuerySnapshot> newStream;
+
 class ViewBlog extends StatefulWidget {
   @override
   _ViewBlogState createState() => _ViewBlogState();
@@ -11,11 +15,14 @@ class _ViewBlogState extends State<ViewBlog> {
   @override
   void initState() {
     super.initState();
-    FirebaseFirestore.instance.collection('blog').snapshots();
+    newStream = firestoreInstance.collection('blog').snapshots();
   }
 
   @override
   Widget build(BuildContext context) {
+    var _screenWidth = MediaQuery.of(context).size.width;
+    var _width = (_screenWidth - 20) / 2;
+    var _aspectRatio = _width / 250;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -27,8 +34,49 @@ class _ViewBlogState extends State<ViewBlog> {
         ),
       ),
       body: Container(
-        child: Column(
-          children: [],
+        child: Container(
+          child: StreamBuilder(
+            stream: newStream,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              var totalgroupCount = 0;
+              List<DocumentSnapshot> groupUsers;
+              if (snapshot.hasData) {
+                groupUsers = snapshot.data.documents;
+                totalgroupCount = groupUsers.length;
+                return (Container(
+                  child: ListView.builder(
+                      itemCount: groupUsers.length,
+                      itemBuilder: (context, int index) {
+                        return SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          color: Colors.blue),
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.2,
+                                      width:
+                                          MediaQuery.of(context).size.width * 1,
+                                      child: Text(
+                                          groupUsers[index].data()['time'])),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ));
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
       ),
     );
